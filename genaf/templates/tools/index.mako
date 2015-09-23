e146f99a6833a229e821973cf135c95c5e6d512e
Hidayat Trimarsanto <anto@eijkman.go.id>
2015-09-23 15:19:28 +0700

    modified analysis form layout

--- genaf/templates/tools/index.mako
+++ genaf/templates/tools/index.mako
@@ -3,6 +3,39 @@
 <h3>${header_text}</h3>
 
 <div class='row'><div class='col-md-8'>
-${queryform}
-</div></div>
 
+<ul class='nav nav-tabs' role='tablist'>
+  <li role='presentation' class='active'>
+    <a href="#form_pane" aria-controls='form_pane' role='tab' data-toggle='tab'>Form Query</a>
+  </li>
+  <li role='presentation'>
+    <a href="#yaml_pane" aria-controls='yaml_pane' role='tab' data-toggle='tab'>YAML Query</a>
+  </li>
+</ul>
+
+<div class='tab-content'>
+
+  <div role='tabpanel' class='tab-pane active' id='form_pane'>
+
+    <div class='row'><div class='col-md-12'>
+      &nbsp;
+      ${queryform}
+    </div></div>
+  </div>
+
+  <div role='tabpanel' class='tab-pane' id='yaml_pane'>
+
+    <div class='row'><div class='col-md-12'>
+      &nbsp;
+      ${yamlform}
+    </div></div>
+  </div>
+
+</div>
+
+</div></div>
+##
+##
+<%def name="jscode()">
+${code | n}
+</%def>
--- genaf/views/tools/__init__.py
+++ genaf/views/tools/__init__.py
@@ -11,14 +11,16 @@ def basic_query_form(request):
     # samples
     batches = list( dbh.get_batches(groups = request.user.groups) )
     qform.add(
-        fieldset()[
+        fieldset(name='simple_query')[
             input_select(name='batches', label='Batch code(s)', multiple = True,
-                    options = [ (b.id, b.code) for b in batches ]),
-            input_text(name='queryset', label='Query set'),
+                    options = [ (b.id, b.code) for b in batches ],
+                    extra_control = '<a id="show_syntax_query">Use query set</a>'),
+        ],
+        fieldset(name='syntax_query', style="display: none;")[
+            input_textarea(name='queryset', label='Query set', disabled='disabled',
+                    extra_control = '<a id="show_simple_query">Use query form</a>'),
         ]
     )
-
-    qform.get('queryset').add_error('Optional, can be left blank')
 
     # markers
     markers = list( dbh.get_markers() )
@@ -26,7 +28,8 @@ def basic_query_form(request):
     qform.add(
         fieldset()[
             input_select(name='markers', label='Marker(s)', multiple = True,
-                    options = [ (m.id, m.label) for m in markers ]),
+                    options = [ (m.id, m.label) for m in markers ],
+                    extra_control = '<a id="markers_clear">Clear</a>'),
         ]
     )
 
@@ -34,15 +37,16 @@ def basic_query_form(request):
     qform.add(
         fieldset()[
             input_text(name='allele_abs_treshhold', label='Allele absolute threshold',
-                        value=100),
+                        value=100, size=2,
+                        info = "The minimum absolute rfu value for each peak to be considered as a real peak"),
             input_text(name='allele_rel_threshold', label='Allele relative threshold',
-                        value=0.33),
+                        value=0.33, size=2),
             input_text(name='allele_rel_cutoff', label='Allele relative cutoff',
-                        value=0.00),
+                        value=0.00, size=2),
             input_text(name='sample_qual_threshold', label='Sample quality threshold',
-                        value=0.50),
+                        value=0.50, size=2),
             input_text(name='marker_qual_threshold', label='Marker quality threshold',
-                        value=0.10)
+                        value=0.10, size=2)
         ]
     )
 
@@ -53,6 +57,38 @@ def basic_query_form(request):
     return qform
 
 
+def jscode():
+
+    return '\n'.join([
+            "$('#batches').select2();",
+            "$('#markers').select2();",
+            "$('#markers_clear').on('click', function() { $('#markers').val(null).trigger('change'); });",
+            "$('#show_syntax_query').on('click', function() {"
+                "$('#syntax_query').show(); $('#queryset').prop('disabled', false); "
+                "$('#simple_query').hide(); });",
+            "$('#show_simple_query').on('click', function() {"
+                "$('#syntax_query').hide(); $('#queryset').prop('disabled', true); "
+                "$('#simple_query').show(); });",
+
+        ])
+
+def yaml_query_form(request):
+
+    yform = form(name='yamlform', action='#')
+    yform.add(
+        fieldset()[
+            input_textarea(name="yamlquery", label='YAML query', size='8x8'),
+        ]
+    )
+
+    yform.add(
+        fieldset()[ submit_bar('Execute', '_yamlexec')]
+    )
+
+    return yform
+
+
+
 def process_request( request, header_text, button_text, callback ):
 
     if not request.GET.get('_method', None) in ['_exec', '_yamlexec']:
@@ -60,6 +96,8 @@ def process_request( request, header_text, button_text, callback ):
         return render_to_response('genaf:templates/tools/index.mako',
             {   'header_text': header_text,
                 'queryform': basic_query_form(request),
+                'code': jscode(),
+                'yamlform': yaml_query_form(request),
             }, request = request)
 
 

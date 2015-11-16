7e04b96046fd21a48d2c988d7c8b2d09423da37d
Hidayat Trimarsanto <anto@eijkman.go.id>
2015-11-16 09:01:32 +0700

    added working code for MCA tools

--- genaf/views/tools/mca.py
+++ genaf/views/tools/mca.py
@@ -4,15 +4,45 @@ from genaf.views.tools import *
 
 from rhombus.lib import fsoverlay as fso
 
+from genaf.views.tools.pcoa import format_output
+
+from itertools import combinations
 
 @roles(PUBLIC)
 def index(request):
-    pass
+
+    return process_request( request, 'Multiple Correspondence Analysis (MCA) Result',
+        'Generate MCA', callback = func_callback)
 
 
 def func_callback( query, request ):
-    pass
 
+    from fatools.lib.analytics.haploset import get_haplotype_sets
+    from fatools.lib.analytics.dist import get_distance_matrix, null_distance
+    from fatools.lib.analytics.ca import mca, plot_pca
 
-def format_output(results, options=None):
-    pass
+    dimension = 2
+
+    analytical_sets = query.get_filtered_analytical_sets()
+    haplotype_sets = get_haplotype_sets(analytical_sets)
+    dm = get_distance_matrix(haplotype_sets, null_distance)
+    mca_res = mca(dm)
+
+    fso_dir = get_fso_temp_dir(request.user.login)
+    plotfile_urls = []
+
+    for (ax, ay) in combinations(range( dimension ), 2):
+        plotfile = fso_dir.abspath + '/' + 'pcoa-%d-%d' % (ax, ay)
+        plot_png = plot_pca(mca_res, dm, ax, ay, plotfile + '.png')
+        plot_pdf = plot_pca(mca_res, dm, ax, ay, plotfile + '.pdf')
+        plotfile_urls.append( (fso.get_urlpath(plot_png), fso.get_urlpath(plot_pdf)) )
+
+    options = { 'plotfile_urls': plotfile_urls }
+
+    html, code = format_output( (mca_res, dm), options )
+
+    return render_to_response("genaf:templates/tools/report.mako",
+            {   'header_text': 'Multiple Correspondence Analysis (MCA) Result',
+                'html': html,
+                'code': code,
+            }, request = request )

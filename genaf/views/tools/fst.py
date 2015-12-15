6003b1180f7c15eda312777f36ac8c507a935ecc
Hidayat Trimarsanto <anto@eijkman.go.id>
2015-12-15 09:21:04 +0700

    added working FST tool

--- genaf/views/tools/fst.py
+++ genaf/views/tools/fst.py
@@ -1,13 +1,76 @@
 
 from genaf.views.tools import *
 
+## FST calculation, uses Arlequin
+
 @roles(PUBLIC)
 def index(request):
 
-    return process_request( request, 'Haplotype Summary', 'Summarize haplotypes',
+    return process_request( request, 'FST', 'Calculate FST',
             callback = func_callback )
 
 
 def func_callback( query, request ):
 
-    pass
+    from fatools.lib.analytics.fst_arlequin import run_arlequin, standardized_fst
+
+    dbh = get_dbhandler()
+    analytical_sets = query.get_filtered_analytical_sets()
+
+    # prepare the directory
+
+    fso_dir = get_fso_temp_dir(request.user.login)
+    fst = run_arlequin( analytical_sets, dbh,  tmp_dir = fso_dir.abspath)
+    fst_max = run_arlequin( analytical_sets, dbh, tmp_dir = fso_dir.abspath, recode=True)
+    fst_std = standardized_fst( fst, fst_max )
+
+    html, code = format_output( fst, fst_max, fst_std )
+
+    return render_to_response("genaf:templates/tools/report.mako",
+            {   'header_text': 'FST Calculation Result',
+                'html': html,
+                'code': code,
+            }, request = request )
+
+
+def format_output( fst, fst_max, fst_std ):
+
+    body = div()
+
+    body.add( h4('FST') )
+    body.add( create_table( fst ) )
+
+    body.add( h4('FST Maximum') )
+    body.add( create_table( fst_max ) )
+
+    body.add( h4('FST Standardized') )
+    body.add( create_table( fst_std ) )
+
+    return (body, '')
+
+
+def create_table( fst_result ):
+
+    header_row = tr()[ th('X') ]
+    for label in fst_result.get_labels():
+        header_row.add( th(label) )
+
+    t = table(class_='table table-condensed')[
+        thead()[ header_row ]
+    ]
+
+    body = tbody()
+    for i in range(len(fst_result.fst_m)):
+        row = tr()[ td( fst_result.labels[i+1] ) ]
+
+        for val in fst_result.fst_m[i]:
+            row.add( td('%s' % val) )
+
+        body.add( row )
+
+    t.add( body )
+
+    return t
+
+
+    raise RuntimeError

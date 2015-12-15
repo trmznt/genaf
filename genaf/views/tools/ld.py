62fd2b1993152b5e3f482aeeb4cd5850e39588e7
Anto - alrescha <anto@eijkman.go.id>
2015-12-15 12:40:14 +0700

    added LD tools

--- genaf/views/tools/ld.py
+++ genaf/views/tools/ld.py
@@ -3,10 +3,42 @@ from genaf.views.tools import *
 @roles(PUBLIC)
 def index(request):
 
-    return process_request( request, 'Haplotype Summary', 'Summarize haplotypes',
+    return process_request( request, 'LD (lian - LInkage ANalysis)', 'Calculate LD',
             callback = func_callback )
 
 
 def func_callback( query, request ):
 
-    pass
+    from fatools.lib.analytics.ld_lian import run_lian
+
+    dbh = get_dbhandler()
+    analytical_sets = query.get_filtered_analytical_sets()
+
+    # using LIAN, we don't have to prepare temporary directory
+
+    results = run_lian(analytical_sets, dbh)
+
+    html, code = format_output(results)
+
+    return render_to_response("genaf:templates/tools/report.mako",
+            {   'header_text': 'LD Calculation Result',
+                'html': html,
+                'code': code,
+            }, request = request )
+
+
+def format_output(results):
+
+    body = div()
+
+    for (label, lian_result) in results:
+
+        t = table(class_='table table-condensed table-striped')[
+                tr()[ td('LD'), td(lian_result.get_LD()) ],
+                tr()[ td('p-value'), td(lian_result.get_pvalue())],
+        ]
+
+        body.add( h3(label), t )
+
+    return div(class_='row')[ div(class_='col-md-12')[ body ] ], ''
+

b8d55123b5ade1405945fb6b9937efd6b6f8365c
Hidayat Trimarsanto <anto@eijkman.go.id>
2016-05-03 02:12:30 +0700

    added custom tip labeling

--- genaf/views/tools/nj.py
+++ genaf/views/tools/nj.py
@@ -2,11 +2,32 @@ from genaf.views.tools import *
 
 from rhombus.lib import fsoverlay as fso
 
+
 @roles(PUBLIC)
 def index(request):
 
     return process_request( request, 'Neighbor-Joining Tree (NJ)', 'Generate NJ',
-            callback = func_callback )
+            callback = func_callback, form_modifier = form_modifier )
+
+
+def form_modifier(html, javacode):
+    """ add tip labeling form """
+    field_set = html.get('additional_fields')
+    field_set.add(
+        input_select(name='tip_label', label='Tip label', value='S',
+            options = [ ('S', 'Sample Code'),
+                        ('I', 'Sample ID'),
+                        ('C', 'Country'),
+                        ('1', 'Administrative Level 1'),
+                        ('2', 'Administrative Level 2'),
+                        ('3', 'Administrative Level 3'),
+                        ('4', 'Administrative Level 4'),
+                    ]
+            )
+    )
+
+    return html, javacode
+
 
 
 def func_callback( query, request ):
@@ -21,10 +42,21 @@ def func_callback( query, request ):
     dbh = get_dbhandler()
     fso_dir = get_fso_temp_dir(request.user.login)
 
+    tip_label = request.params.get('tip_label', 'S')
+    label_callback = {
+        'S': lambda x: dbh.get_sample_by_id(x).code,
+        'I': None,
+        'C': lambda x: dbh.get_sample_by_id(x).location.country,
+        '1': lambda x: dbh.get_sample_by_id(x).location.level1,
+        '2': lambda x: dbh.get_sample_by_id(x).location.level2,
+        '3': lambda x: dbh.get_sample_by_id(x).location.level3,
+        '4': lambda x: dbh.get_sample_by_id(x).location.level4,
+    }
+
     njplot_png = plot_nj(dm, fso_dir.abspath, 'png',
-            label_callback = lambda x: dbh.get_sample_by_id(x).code)
+            label_callback = label_callback[tip_label])
     njplot_pdf = plot_nj(dm, fso_dir.abspath, 'pdf',
-            label_callback = lambda x: dbh.get_sample_by_id(x).code)
+            label_callback = label_callback[tip_label])
 
     options = { 'png_plot': fso.get_urlpath(njplot_png),
                 'pdf_plot': fso.get_urlpath(njplot_pdf) }
@@ -40,7 +72,8 @@ def format_output( options ):
 
     if options:
         html.add(
-                image(src=options['png_plot'])
+                image(src=options['png_plot']),
+                p(a('Click here to get the plot as a PDF file!', href=options['pdf_plot']))
                 )
 
     return (html, '')

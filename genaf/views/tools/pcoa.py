9780ba7c38b714b7fd5ae08626be2fdd32f7f418
Anto - alrescha <anto@eijkman.go.id>
2016-08-02 13:08:46 +0700

    added data download link in result

--- genaf/views/tools/pcoa.py
+++ genaf/views/tools/pcoa.py
@@ -15,7 +15,7 @@ def index(request):
 def func_callback( query, user ):
 
     from fatools.lib.analytics.dist import get_distance_matrix
-    from fatools.lib.analytics.ca import pcoa, plot_pca
+    from fatools.lib.analytics.ca import pcoa, plot_pca, format_data
 
     dimension = 2
 
@@ -33,7 +33,14 @@ def func_callback( query, user ):
         plot_pdf = plot_pca(pca_res, dm, ax, ay, plotfile + '.pdf')
         plotfile_urls.append( (fso.get_urlpath(plot_png), fso.get_urlpath(plot_pdf)) )
 
-    options = { 'plotfile_urls': plotfile_urls }
+    pca_data = format_data(pca_res, dm)
+    data_file = fso_dir.abspath + '/' + 'pcoa-data.txt'
+    with open(data_file, 'w') as outfile:
+        for r in pca_data:
+            outfile.write( '\t'.join( r ) )
+            outfile.write( '\n' )
+
+    options = { 'plotfile_urls': plotfile_urls, 'data_file': fso.get_urlpath(data_file) }
 
     html, code = format_output( (pca_res, dm), options )
 
@@ -56,6 +63,12 @@ def format_output( results, options ):
             html.add(
                 image(src=png)
                 )
+
+    html.add(
+        br(),
+        'Download data file here: ',
+        a('data.txt', href=options['data_file']),
+    )
 
     return (html, '')
 
